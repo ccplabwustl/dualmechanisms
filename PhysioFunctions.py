9072e15211fe27991f11b21a80c8c28bf727f6a0
mitchem890@gmail.com <mitchem890@gmail.com>
2017-09-15 11:57:32 -0500

    removed DCM downloader and changed functionality from finding local files from sourcing all info from intradb.

--- PhysioFunctions.py
+++ PhysioFunctions.py
@@ -1,37 +1,78 @@
 import os
-import shutil
 import csv
 # Get the User input for each parameter
 def getUserInput():
 
-    DIR = raw_input("Enter the directory where your \'DMCC_Phase2(HCP)\' is located: ")
-    #PROJ = raw_input('Enter DMCC_Phase2 or DMCC Phase3: ')
-    PROJ = 'DMCC_Phase2'
-    SUBJ = str(input('Enter the subject number: '))
+
+
+    PROJ = raw_input('Enter DMCC_Phase2 or DMCC Phase3: ')
+    SUBJ = raw_input('Enter the subject number: ')
     SESS = raw_input('Enter the session: ')
 
-    return DIR, PROJ, SUBJ, SESS
+    return PROJ, SUBJ, SESS
 
-#Get the list of physio Files and store in to tmp.csv
-def GetPhysioData(directory, project, subject, session):
-    os.system("curl -s -k -n https://intradb.humanconnectome.org/data/projects/" + project + "/subjects/" \
-              + subject + "/experiments/" + subject + "_" + session + \
-              "/scans?format=csv | grep \"Physio\" | cut -d, -f7,8 > " + os.path.join(directory, 'tmp.csv'))
+
+#Get the list of trial with physio data, along with scan numbers like the following:
+#tfMRI_CuedtsBas1_AP_PhysioLog,20
+#tfMRI_CuedtsBas2_PA_PhysioLog,23
+#tfMRI_StroopTest_AP_PhysioLog,26
+#tfMRI_StroopBas1_AP_PhysioLog,29
+def GetTrialInfo(directory, project, subject, session):
+    filename = subject + '_' + session + '_' + 'trial.csv'
+    os.system('bash TrialWithScanRequest.sh -s ' + subject + ' -e ' + session + ' -p ' + project + ' -d ' + directory)
+
+    try:
+        with open(os.path.join(directory, filename), mode='r') as infile:
+            reader = csv.reader(infile)
+            fileDict = {rows[1]: rows[0] for rows in reader}
+
+    except:
+        print 'ISSUE with csv:' + filename
+    os.remove(os.path.join(directory, filename))
+    return fileDict
+
+
+#makes a CSVs with the UUIDS and scan Numbers then place those values into a
+def GetUUIDInfo(directory, project, subject, session):
+    filename = subject+'_'+session+'_'+'UUIDS.csv'
+    os.system('bash ScanNumWithUUIDRequest.sh -s ' + subject + ' -e ' + session + ' -p ' + project + ' -d ' + directory)
+    #Create a Dictionary out of the csv with the
+    try:
+        with open(os.path.join(directory, filename), mode='r') as infile:
+            reader = csv.reader(infile)
+            fileDict = {rows[0]: rows[1] for rows in reader}
+
+    except:
+        print 'ISSUE with csv:' + filename
+    os.remove(os.path.join(directory, filename))
+    return fileDict
+
+    return scanUUID
+
+
+def DictCleanup(dict1, ABV):
+    # remove all rest and StroopTest physio files
+    dict1 = {key: value for key, value in dict1.items()
+            if ('Rest' not in key and 'Test' not in key)}
+    # Generalize Keys to make names more universal, by removing the Abreviation
+    for key, value in dict1.items():
+        dict1[key[6:].replace(ABV, '').replace('_PhysioLog', '')] = dict1.pop(key)
+    return dict1
+
 
 #merges the dictionaries to using the Keys in dict1 and the values from dict2
 def mergeDictionaries(dict1, dict2):
     dictMerge = {}
-    print dict1
-    print '\n\n'
-    print dict2
     for key, value in dict1.items():
         dictMerge[key] = dict2[dict1[key]]
     return dictMerge
+
 
 #Finds the longest value of the dictionary and returns the length
 def FindMaxLengthValue(dictionary):
     keyLongestName = max(dictionary, key = lambda k: len(dictionary[k]))
     return len(dictionary[keyLongestName])
+
 
 # Build Matrix String based on
 # runnames = [['Axcpt' sessidshort '1_AP ']; ['Axcpt' sessidshort '2_PA '];
@@ -62,56 +103,3 @@ def BuildMatrix(dict1):
     runnames = runnames + '];\n'
 
     return uuidMatrix, runnames
-
-#Removes IntraDB file structure while looking at the scan numbers and file names to build a
-def BuildDCMDict(directory, subject, session ):
-    DCMDict = {}
-    #Find Files and place in a dictionary with their scan number
-    #Make a dictionary With a Key of scan Number and a value of Filename
-    #move the files to the parent directory for the matlab script
-    scansPath = os.path.join(directory, subject+'_'+session,'scans')
-    for directories in os.listdir(scansPath):
-        for root, dir, files in os.walk(os.path.join(scansPath,directories)):
-            for name in files:
-                DCMDict[directory[:2]] = os.path.splitext(name)[0]
-                shutil.copy(os.path.join(root, name), os.path.join(directory, name))
-    shutil.rmtree(os.path.join(directories, subject+'_'+session))
-    return DCMDict
-
-#Download the physio files to the directory folder
-def DownloadDCMFiles(directory, project, subject, session):
-    print 'Downloading Physio Data from intraDB:'
-    os.system('bash intraDBPhysioDownload.sh -s ' + subject + ' -e ' + session + ' -p ' + project + ' -d ' + directory)
-
-
-
-#makes a CSVs with the UUIDS and scan Numbers then place those values into a
-def findUUIDs(directory, project, subject, session):
-
-    # #request the list of UUIDs and scan numbers from intraDB
-    # # then trim the list to only include lines that are scans with grep \'scans\'
-    # # then trim any lines that are missing parameters with grep -Ev $\'^,|,,|,$\'
-    # # then remove all the SBRef scans with grep -v \'SBRef\'
-    # # then leave only fields 2 and 6 in the csv with cut -d, -f2,6
-    # # finally remove duplicates with awk -F, \'!seen[$1]++\'
-    os.system('curl -k -n https://intradb.humanconnectome.org/data/projects/'+project+'/subjects/'+ subject+'/experiments/'+\
-    subject+'_'+session+'/scans?format=csv\&columns=xnat:imageScanData/image_session_ID,ID,type,series_description,'+\
-    'xnat:mrScanData/fileNameUUID,URI |grep \'scans\'|grep -Ev $\'^,|,,|,$\' | grep -v \'SBRef\' | cut -d, -f2,6 > \''+\
-    os.path.join(directory,'UUIDS.csv') + '\'')
-
-    #Create a Dictionary out of the csv with the
-    with open(os.path.join(directory, 'UUIDS.csv'), mode='r') as infile:
-        reader = csv.reader(infile)
-        trialFileDict = {rows[1]: rows[0] for rows in reader}
-    os.remove(os.path.join(directory, 'UUIDS.csv'))
-
-    return trialFileDict
-
-def DictCleanup(dict1, ABV):
-    # remove all rest and StroopTest physio files
-    dict1 = {key: value for key, value in dict1.items()
-            if ('Rest' not in key and 'Test' not in key)}
-    # Generalize Keys to make names more universal, by removing the Abreviation
-    for key, value in dict1.items():
-        dict1[key[6:].replace(ABV, '').replace('_PhysioLog', '')] = dict1.pop(key)
-    return dict1
--- PhysioHelper.py
+++ PhysioHelper.py
@@ -4,77 +4,28 @@
 # Date: 8/2/17
 
 import PhysioFunctions as PhyFun
-import os
-import glob
 
-# get User Input
+
 #TestValues:
-# DIR = '/home/mitchell/Desktop/'
-# SUBJ = '132017'
+# SUBJ = 'DMCC6671683'
 # SESS = 'baseline'
 # PROJ = 'DMCC_Phase2'
 
 
 #Get the User Input
-DIR, PROJ, SUBJ, SESS = PhyFun.getUserInput()
+PROJ, SUBJ, SESS = PhyFun.getUserInput()
 
 ABV = SESS[:3].capitalize()
 
-# trialIDDict Stores the Key scan number with the Value of the trial name
-# fileNameDict stores the file name associated with the scan number
-trialIDDict = {}
-fileNameDict = {}
-
-#Build the paths for the RawData and Preprocessed Data
-RawDataDir = os.path.join(DIR, PROJ+'(HCP)', 'Raw_Data', SUBJ, SUBJ + '_' + SESS, 'physio_data')
-PreProcDataDir = os.path.join(DIR, PROJ+'(HCP)', 'Preprocessed_Data', SUBJ, 'physio_data')
-
-# Build The file Structure for the Data
-if not os.path.exists(RawDataDir):
-    os.makedirs(RawDataDir)
-
-if not os.path.exists(PreProcDataDir):
-    os.makedirs(PreProcDataDir)
-
-
-# ########### IF YOU ARE USING DCM FILE ##########
-# #Get the Correct file numbers that we need to download
-# PhyFun.GetPhysioData(RawDataDir,PROJ, SUBJ, SESS)
-#
-# # open tmp.csv and store values into a dict where the Key is the the first column "tfMRI_Cuedts"
-# with open(os.path.join(RawDataDir, 'tmp.csv'), mode='r') as infile:
-#     reader = csv.reader(infile)
-#     trialIDDict = {rows[0]: rows[1][-2:] for rows in reader}
-# os.remove(os.path.join(RawDataDir, 'tmp.csv'))
-#
-#
-
-# #Download DCM Files
-# PhyFun.DownloadDCMFiles(RawDataDir, PROJ, SUBJ, SESS)
-# #remove the file structure and build fileNameDict
-# fileNameDict = PhyFun.BuildDCMDict(RawDataDir, SUBJ,SESS)
-
-#Merge the two Dictionaries resulting in Key of trial names and Value of filenames
-#trialFileDict = PhyFun.mergeDictionaries(trialIDDict, fileNameDict)
-########## END DCM SPECIFIC CODE ############
-
-
-
-########## IF YOU ARE USING LINKED_DATA LIKE  _Info.log ##########
-
 #find the uuids and build a dict with key of scan numbers and value of uuid
-trialFileDict = PhyFun.findUUIDs(RawDataDir, PROJ, SUBJ, SESS)
-
-
-#after we have a scan numbers then find the file names with in the Raw_Data Folder that correspond to the UUIDs
-for keys in trialFileDict.keys():
-    os.chdir(RawDataDir)
-    trialFileDict[keys] = glob.glob('Physio_*' + trialFileDict[keys] + '_Info.log')[0][:-9]
-os.chdir(DIR)
-########### END LINKED_DATA SPECIFIC CODE ##########
+trialScanDict = PhyFun.GetTrialInfo('.', PROJ, SUBJ, SESS)
+print trialScanDict
+scanUUIDDict = PhyFun.GetUUIDInfo('.', PROJ, SUBJ, SESS)
+print scanUUIDDict
+trialUUIDDict = PhyFun.mergeDictionaries(trialScanDict, scanUUIDDict)
 
 #Generalize the trial names
-trialFileDict = PhyFun.DictCleanup(trialFileDict, ABV)
+trialFileDict = PhyFun.DictCleanup(trialUUIDDict, ABV)
 
 #build a String of the matrix for the matlab file
 uuids, runnames = PhyFun.BuildMatrix(trialFileDict)
